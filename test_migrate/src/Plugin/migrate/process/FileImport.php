6509f96b1213a30723e77f25847951a731b4cbf8
Sudheesh S <sudheesh.s@zyxware.com>
2018-03-22 14:38:52 +0530

    Working on FileImport plugin.

---
+++ test_migrate/src/Plugin/migrate/process/FileImport.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\eminent_migrate\Plugin\migrate\process;
+
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+
+/**
+ * Example on how to migrate an image from any place in Drupal.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "file_import"
+ * )
+ */
+class FileImport extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+
+    $user = \Drupal::currentUser();
+    $file = \Drupal::entityTypeManager()->getStorage('file')->create(['uri' => $value]);
+
+    // TODO :: Get properties from csv & set it for the file instance.
+    // Get uri property from csv
+    $file_uri = $row->getSourceProperty('get_the_uri_property_from_csv'); 
+    
+    // Set it for the file object
+    $file->setFileUri();
+    $file->save();
+
+    return $file->id();
+  }
+
+}
